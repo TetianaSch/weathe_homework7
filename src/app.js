637d21144f53b1bbfca927bf3adad9f68e4e8d04
TetianaSch <14tatimo@gmail.com>
2022-11-14 12:40:31 +0100

    js updated

--- src/app.js
+++ src/app.js
@@ -75,6 +75,7 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+
 function search(city) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
